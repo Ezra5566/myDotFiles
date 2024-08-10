2a8cc027e7af2a7aacdb3f495517b0fb65cfe70b
Ezra <ezraakush@gmail.com>
2024-08-10 06:19:18 -0700

    Create PRESETS_for_chromeDEV.js

---
+++ hack/nerdfont/PRESETS_for_chromeDEV.js
@@ -0,0 +1,26 @@
+term_.prefs_.set('font-family', 'Monofur Nerd Font, monospace');
+term_.prefs_.set('user-css-text', 
+                 `@font-face {
+                     font-family: "Monofur Nerd Font"; 
+                     src: url("http://localhost:8080/MonofurNerdFont-Regular.ttf"); 
+                     font-weight: normal; 
+                     font-style: normal;
+                  }
+                  x-row {
+                      text-rendering: optimizeLegibility;
+                      font-variant-ligatures: normal;
+                  }`);
+
+
+term_.prefs_.set('font-family', 'JetBrains Mono Nerd Font, monospace');
+term_.prefs_.set('user-css-text', 
+                 `@font-face {
+                     font-family: "JetBrains Mono Nerd Font"; 
+                     src: url("http://localhost:8080/JetBrainsMonoNerdFont-Regular.ttf"); 
+                     font-weight: normal; 
+                     font-style: normal;
+                  }
+                  x-row {
+                      text-rendering: optimizeLegibility;
+                      font-variant-ligatures: 
+
