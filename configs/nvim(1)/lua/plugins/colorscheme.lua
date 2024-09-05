392e227aa7d967b58e7b9abae605fa4461cb3403
Ezra <ezraakush@gmail.com>
2024-09-05 15:13:03 +0300

    Update colorscheme.lua
    changed default color scheme structure

--- configs/nvim(1)/lua/plugins/colorscheme.lua
+++ configs/nvim(1)/lua/plugins/colorscheme.lua
@@ -1,169 +1,21 @@
 return {
+  -- Default colorscheme (gruvbox)
   {
     "ellisonleao/gruvbox.nvim",
-    lazy = false, -- load this during startup
-    priority = 1000, -- ensure it loads before other plugins
+    lazy = false, -- Load on startup
+    priority = 1000, -- Make sure it's loaded first
     config = function()
       require("gruvbox").setup({
         transparent_mode = true,
         overrides = {
-          String = { italic = true },
+          String = { italic = false },
         },
       })
-      vim.cmd([[colorscheme gruvbox]]) -- set gruvbox as the colorscheme
-    end,
-  },
-  -- main colorscheme
-  -- solarized
-  {
-    "ishan9299/nvim-solarized-lua",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    dir = "~/personal/nvim-solarized-lua",
-    config = function()
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "solarized",
-        callback = function()
-          vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-          vim.api.nvim_set_hl(0, "LineNr", { fg = "#586e75", bg = "none" })
-          vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#b58900", bg = "none" })
-          vim.api.nvim_set_hl(0, "CursorLine", { fg = "none", bg = "#002b36" })
-          vim.api.nvim_set_hl(0, "Visual", { fg = "#002b36", bg = "#586e75" })
-          vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#dc322f", bg = "#360909" })
-          vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { fg = "#b58900", bg = "#1c1500" })
-          vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { fg = "#268bd2", bg = "#0e3550" })
-          vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { fg = "#2aa198", bg = "#0a2725" })
-          vim.api.nvim_set_hl(0, "DiagnosticInfo", { fg = "#268bd2", bg = "none" })
-          vim.api.nvim_set_hl(0, "DiagnosticHint", { fg = "#2aa198", bg = "none" })
-          vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#586e75" })
-          vim.api.nvim_set_hl(0, "LazyButton", { link = "Visual" })
-          vim.api.nvim_set_hl(0, "LazyButtonActive", { link = "IncSearch" })
-          vim.api.nvim_set_hl(0, "AlphaButtons", { link = "Conceal" })
-          vim.api.nvim_set_hl(0, "AlphaHeader", { link = "Debug" })
-          vim.api.nvim_set_hl(0, "AlphaShortcut", { italic = true, fg = "#859900" })
-          vim.api.nvim_set_hl(0, "AlphaFooter", { link = "String" })
-          vim.api.nvim_set_hl(0, "CurSearch", { link = "IncSearch" })
-          vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, sp = "#2aa198" })
-          vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { undercurl = true, sp = "#268bd2" })
-          vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = "#b58900" })
-          vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "#dc322f" })
-          vim.api.nvim_set_hl(0, "@parameter", { link = "Variable" })
-          vim.api.nvim_set_hl(0, "@variable.parameter", { link = "Variable" })
-          vim.api.nvim_set_hl(0, "@lsp.type.parameter", { link = "Variable" })
-          vim.cmd("highlight GitSignsAdd guibg=none")
-          vim.cmd("highlight GitSignsChange guibg=none")
-          vim.cmd("highlight GitSignsDelete guibg=none")
-        end,
-        group = vim.api.nvim_create_augroup("FixSolarized", { clear = true }),
-        desc = "Fix some highlight for solarized colorscheme",
-      })
-
-      -- temporory disable semantic tokens highlight,
-      -- since ishan9299/nvim-solarized-lua not support it yet
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          client.server_capabilities.semanticTokensProvider = nil
-        end,
-      })
-
-      -- vim.g.solarized_termtrans = 1
-      -- vim.cmd("colorscheme solarized")
+      -- vim.cmd("colorscheme gruvbox") -- Uncomment to set as default
     end,
   },
 
-  {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = true,
-    keys = {
-      { "<leader>cl", "<cmd>Telescope colorscheme<cr>", desc = "Select Colorscheme with Preview" },
-    },
-    config = function()
-      require("solarized-osaka").setup({
-        transparent = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-        on_highlights = function(highlights, colors)
-          highlights.Visual = {
-            bg = colors.fg,
-            fg = colors.bg,
-          }
-          highlights.AlphaButtons = {
-            link = "Conceal",
-          }
-          highlights.AlphaHeader = {
-            link = "Debug",
-          }
-          highlights.AlphaShortcut = {
-            link = "@keyword",
-          }
-        end,
-      })
-      local colors = require("solarized-osaka.colors").default
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "solarized-osaka",
-        callback = function()
-          vim.api.nvim_set_hl(0, "Visual", { bg = colors.fg, fg = colors.bg })
-          vim.api.nvim_set_hl(0, "AlphaButtons", { link = "Conceal" })
-          vim.api.nvim_set_hl(0, "AlphaHeader", { link = "Debug" })
-          vim.api.nvim_set_hl(0, "AlphaShortcut", { link = "@keyword" })
-        end,
-        group = vim.api.nvim_create_augroup("FixSolarizedOsaka", { clear = true }),
-        desc = "Fix some highlight for solarized-osaka colorscheme",
-      })
-
-      -- vim.cmd([[colorscheme solarized-osaka]])
-    end,
-  },
-
-  -- gruvbox-material
-  {
-    "sainnhe/gruvbox-material",
-    lazy = true,
-    keys = {
-      { "<leader>cl", "<cmd>Telescope colorscheme<cr>", desc = "Select Colorscheme with Preview" },
-    },
-    config = function()
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "gruvbox-material",
-        callback = function()
-          vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-          vim.api.nvim_set_hl(0, "GruvboxYellowSign", { link = "GruvboxYellow" })
-          vim.api.nvim_set_hl(0, "GruvboxPurpleSign", { link = "GruvboxPurple" })
-          vim.api.nvim_set_hl(0, "GruvboxOrangeSign", { link = "GruvboxOrange" })
-          vim.api.nvim_set_hl(0, "GruvboxGreenSign", { link = "GruvboxGreen" })
-          vim.api.nvim_set_hl(0, "GruvboxBlueSign", { link = "GruvboxBlue" })
-          vim.api.nvim_set_hl(0, "GruvboxAquaSign", { link = "GruvboxAqua" })
-          vim.api.nvim_set_hl(0, "GruvboxRedSign", { link = "GruvboxRed" })
-        end,
-        group = vim.api.nvim_create_augroup("FixGruvboxMaterial", { clear = true }),
-        desc = "Fix some highlight for gruvbox-material colorscheme",
-      })
-      vim.g.gruvbox_material_transparent_background = 1
-    end,
-  },
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = true, -- load this during startup
-    priority = 1000, -- ensure it loads before other plugins
-    config = function()
-      require("cyberdream").setup({
-        transparent_mode = true,
-        overrides = {
-          String = { italic = true },
-        },
-      })
-      keys =
-        { "<leader>cl", "<cmd>Telescope colorscheme<cr>", desc = "Select Colorscheme with Preview" },
-        vim.cmd([[colorscheme cyberdream]]) -- set gruvbox as the colorscheme
-    end,
-  },
-  -- tokyonight
-  -- nightfox
-  -- kanagawa
-  -- catppuccin
+  -- -------------------------Extra colorscheme: catppuccin -----------------------------
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -175,10 +27,67 @@ return {
       require("catppuccin").setup({
         transparent_background = true,
       })
+      -- vim.cmd("colorscheme catppuccin") -- Uncomment to set as default
     end,
   },
-  -- vscode
-  -- moonbow
-  -- darcula (JetBrains Intellij IDEA default theme)
-  -- astrotheme
+
+  -- -------------------------Extra colorscheme: gruvbox-material -----------------------------
+  {
+    "sainnhe/gruvbox-material",
+    lazy = true,
+    keys = {
+      { "<leader>cl", "<cmd>Telescope colorscheme<cr>", desc = "Select Colorscheme with Preview" },
+    },
+    config = function()
+      vim.g.gruvbox_material_transparent_background = 1
+      -- vim.cmd("colorscheme gruvbox-material") -- Uncomment to set as default
+    end,
+  },
+
+  -- -------------------------Extra colorscheme: sonokai -----------------------------
+  {
+    "sainnhe/sonokai",
+    lazy = true,
+    keys = {
+      { "<leader>cl", "<cmd>Telescope colorscheme<cr>", desc = "Select Colorscheme with Preview" },
+    },
+    config = function()
+      vim.g.sonokai_transparent_background = 1
+      -- vim.cmd("colorscheme sonokai") -- Uncomment to set as default
+    end,
+  },
+
+  -- -------------------------Extra colorscheme: edge -----------------------------
+  {
+    "sainnhe/edge",
+    lazy = true,
+    keys = {
+      { "<leader>cl", "<cmd>Telescope colorscheme<cr>", desc = "Select Colorscheme with Preview" },
+    },
+    config = function()
+      vim.g.edge_transparent_background = 1
+      -- vim.cmd("colorscheme edge") -- Uncomment to set as default
+    end,
+  },
+
+  -- -------------------------Extra colorscheme: cyberdream -----------------------------
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = true,
+    keys = {
+      { "<leader>cl", "<cmd>Telescope colorscheme<cr>", desc = "Select Colorscheme with Preview" },
+    },
+    config = function()
+      vim.g.cyberdream_transparent_background = 1
+      require("cyberdream").setup({
+        transparent = true,
+        italic_comments = true,
+        hide_fillchars = true,
+        terminal_colors = false,
+        cache = true,
+        borderless_telescope = { border = false, style = "flat" },
+      })
+      vim.cmd("colorscheme cyberdream") -- Uncomment to set as default
+    end,
+  },
 }
